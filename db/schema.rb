8d75bd2f071bd708383a1dad5c6b9b79e73f2435
Cronwel Noel Irias <noelirias@gmail.com>
2018-03-06 12:38:12 -0500

    installed GEM: foreman, to run both rails and node server. Error in codebase, unable to run servers. FIX- downgrade to webpack-dev-server@^2.11.1 per Stackoverflow solution. Will need to revise later once UTD solution is available

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306163521) do
+ActiveRecord::Schema.define(version: 20180306170135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20180306163521) do
     t.string "title"
     t.string "section"
     t.string "section_appendix"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
