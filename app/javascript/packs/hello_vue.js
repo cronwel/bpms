1c8fedab91ab2ee3aa7cedc33d69e518e99a7773
Cronwel Noel Irias <noelirias@gmail.com>
2018-03-06 13:53:15 -0500

    testing TurbolinksAdapter: functionality test

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,23 +1,18 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
-// to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
+import Vue from 'vue';
+import App from '../app.vue';
+import TurbolinksAdapter from 'vue-turbolinks';
 
-import Vue from 'vue'
-import App from '../app.vue'
+Vue.use(TurbolinksAdapter);
 
 document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'))
+  const el = document.body.appendChild(document.createElement('hello'));
   const app = new Vue({
     el,
     render: h => h(App)
-  })
+  });
 
-  console.log(app)
-})
-
+  console.log(app);
+});
 
 // The above code uses Vue without the compiler, which means you cannot
 // use Vue to target elements in your existing html templates. You would
@@ -31,7 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
 //   {{message}}
 //   <app></app>
 // </div>
-
 
 // import Vue from 'vue/dist/vue.esm'
 // import App from '../app.vue'
